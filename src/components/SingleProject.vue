d463bf24709e6b69b29d344ed492be236b8c0bca
NNEUID <nneuid@gmail.com>
2023-01-23 23:35:15 +0000

    install json-server and add data/db.json res

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3>{{ project.title }}</h3>
+    </div>
+    <div class="details">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+<script>
+/* eslint-disable */
+export default {
+  props: ["project"],
+};
+</script>
+<style>
+.project {
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, .05);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,31 @@
+<!-- eslint-disable -->
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from "../components/SingleProject.vue";
 export default {
   name: "HomeView",
+  components: {
+    SingleProject,
+  },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((error) => console.log(error.message));
+  },
 };
 </script>
