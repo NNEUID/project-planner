e27115e3f144529cd9009c9e701ea18fb16c68cf
NNEUID <nneuid@gmail.com>
2023-01-23 23:45:45 +0000

    add show details challenge

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="project">
-    <div class="actions">
+    <div class="actions" @click="details.show = !details.show">
       <h3>{{ project.title }}</h3>
     </div>
-    <div class="details">
+    <div class="details" v-if="details.show">
       <p>{{ project.details }}</p>
     </div>
   </div>
@@ -12,6 +12,13 @@
 /* eslint-disable */
 export default {
   props: ["project"],
+  data() {
+    return {
+      details: {
+        show: false
+      }
+    }
+  }
 };
 </script>
 <style>
@@ -20,5 +27,6 @@ export default {
   background: white;
   padding: 10px 20px;
   box-shadow: 1px 2px 3px rgba(0, 0, 0, .05);
+  border-left: 4px solid #e90074;
 }
 </style>
