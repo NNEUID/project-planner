34a7e5cc6366c0d4a8f428573dbefc44ab3ca66d
NNEUID <nneuid@gmail.com>
2023-01-26 22:32:07 +0000

    set add project page and form

---
+++ src/views/AddProject.vue
@@ -0,0 +1,24 @@
+<template>
+  <form>
+    <label>Title:</label>
+    <input type="text" required v-model="title">
+    <label>Details:</label>
+    <textarea required v-model="details"></textarea>
+    <button>Add Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      details: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
