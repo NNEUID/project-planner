407ff36398f7e2e79c7a8bd4014a99b0ef3798f9
NNEUID <nneuid@gmail.com>
2023-01-27 23:44:14 +0000

    add css styles to add project view and form

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable -->
 <template>
   <form @submit.prevent="handleSubmit">
     <label>Title:</label>
@@ -7,8 +8,8 @@
     <button>Add Project</button>
   </form>
 </template>
-
 <script>
+/* eslint-disable */
 export default {
   data() {
     return {
@@ -23,7 +24,47 @@ export default {
   }
 }
 </script>
-
 <style>
+form {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
 
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0
+}
+
+input {
+  padding: 10px;
+  border: 0;
+  border-bottom: 1px solid #ddd;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+textarea {
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  height: 100px;
+}
+
+form button {
+  display: block;
+  margin: 20px auto 0;
+  background: #00ce89;
+  color: white;
+  padding: 10px;
+  border: 0;
+  border-radius: 6px;
+  font-size: 16px;
+}
 </style>
