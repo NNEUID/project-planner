80caf93f28bba2c52a56fad56d9d45fec2817f6a
NNEUID <nneuid@gmail.com>
2023-01-29 13:03:40 +0000

    add EditProject view and setup route + dynamically route projects to edit page based on :id params

--- src/views/EditProject.vue
+++ src/views/EditProject.vue
@@ -1,12 +1,27 @@
 <!-- eslint-disable -->
 <template>
-
+  <form>
+    <label>Title:</label>
+    <input type="text" required v-model="title">
+    <label>Details:</label>
+    <textarea required v-model="details"></textarea>
+    <button>Add Project</button>
+  </form>
 </template>
 
 <script>
 /* eslint-disable */
 export default {
-
+  data() {
+    return {
+      data() {
+        return {
+          title: '',
+          details: ''
+        }
+      }
+    }
+  }
 }
 </script>
 
