e4979ac35b673d98bc6f2545597c8eff7be34d9d
NNEUID <nneuid@gmail.com>
2023-02-02 09:31:42 +0000

    add a computed property to filter projects and finalize project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
   <div class="home">
     <FilterNave @filterChange="current = $event" :current="current" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
@@ -25,6 +25,17 @@ export default {
       projects: [],
       current: 'all'
     };
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === 'completed') {
+        return this.projects.filter(project => project.complete)
+      }
+      if (this.current === 'ongoing') {
+        return this.projects.filter(project => !project.complete)
+      }
+      return this.projects
+    }
   },
   methods: {
     handleDelete(id) {
